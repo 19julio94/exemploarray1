e7dceb09bcaac9f937cdce71bebde394c2c76e7c
jpatriciodasilva <jpatriciodasilva@atanasoff06.danielcastelao.org>
2016-02-03 10:34:45 +0100

    final

---
+++ src/exemploarraw1/ExemploArraw1.java
@@ -0,0 +1,17 @@
+
+package exemploarraw1;
+
+
+public class ExemploArraw1 {
+
+    
+    public static void main(String[] args) {
+        Metodos metodos1=new Metodos();
+        metodos1.visualizar();
+        metodos1.crearArray();
+        metodos1.visualizar();
+        
+        
+    }
+    
+}
